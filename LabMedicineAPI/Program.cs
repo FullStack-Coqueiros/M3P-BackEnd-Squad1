e3f0066d2d7d1f7d41fe828b239618082608277d
Vitor Lassen <vitor@lassen.com.br>
2023-10-04 19:39:47 -0300

    ajuste

--- LabMedicineAPI/Program.cs
+++ LabMedicineAPI/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-gggg
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
