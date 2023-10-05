b48089831042bd8dd2bb7916c80df7e0a4641e30
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-04 21:51:22 -0300

    criação consulta model

--- LabMedicineAPI/Model/Consulta.cs
+++ LabMedicineAPI/Model/Consulta.cs
@@ -1,15 +1,29 @@
-﻿namespace LabMedicineAPI.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabMedicineAPI.Model
 {
     public class Consulta
     {
+        [Column(TypeName = "VARCHAR"), Required, MaxLength(64), MinLength(8)]
         public string MotivoConsulta { get; set; }
+        [Required]
         public DateTime DataConsulta { get; set; }
+        [Required]
         public DateTime HorarioConsulta { get; set; }
+        [Column(TypeName = "VARCHAR"), Required, MaxLength(1024), MinLength(16)]
         public string ProblemaDescricao { get; set; }
-        public string MedicacaoIndicada { get; set; }
+        
+        public string? MedicacaoIndicada { get; set; }
+        [Column(TypeName = "VARCHAR"), Required, MaxLength(256), MinLength(16)]
         public string DosagemEPrecaucoes { get; set; }
+        [Required]
         public bool StatusDoSistema { get; set; }
         public int PacienteId { get; set; }
+        [Required]
+        public PacienteModel paciente { get; set; }
         public int UsuarioId { get; set; }
+        [Required]
+        public UsuarioModel usuario { get; set; }
     }
 }
