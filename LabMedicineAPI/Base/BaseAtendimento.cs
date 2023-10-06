83ad1b7fb705051e23e5380e3e6b7c947269fb8e
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-06 15:30:01 -0300

    conferencia do build classe base atendimentos

--- LabMedicineAPI/Base/BaseAtendimento.cs
+++ LabMedicineAPI/Base/BaseAtendimento.cs
@@ -1,6 +1,20 @@
-﻿namespace LabMedicineAPI.Base
+﻿using LabMedicineAPI.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace LabMedicineAPI.Base
 {
     public class BaseAtendimento
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int PacienteId { get; set; }
+        [Required]
+        public PacienteModel Paciente { get; set; }
+        [Required]
+        public int UsuarioId { get; set; }
+        [Required]
+        public UsuarioModel Usuario { get; set; }
+
     }
 }
