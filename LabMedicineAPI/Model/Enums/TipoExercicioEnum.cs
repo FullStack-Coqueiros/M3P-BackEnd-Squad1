454707006e784f8a8aa7784f7e9224fced75b749
alessandramedeirosvieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-10-05 22:25:54 -0300

    criação model exercicio e enum tipoexercicio

---
+++ LabMedicineAPI/Model/Enums/TipoExercicioEnum.cs
@@ -0,0 +1,13 @@
+namespace LabMedicineAPI.Model.Enums
+{
+    public enum TipoExercicioEnum
+    {
+        ResistenciaAerobica,
+        ResistenciaMuscular,
+        Flexibilidade,
+        Forca,
+        Agilidade,
+        Outro
+
+    }
+}
---
+++ LabMedicineAPI/Model/ExercicioModel.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using LabMedicineAPI.Model.Enums;
+
+namespace LabMedicineAPI.Model
+{
+    [Table("Exercicio")]
+    public class ExercicioModel
+    {
+        [Column(TypeName = "VARCHAR"), Required, MaxLength(100), MinLength(5)]
+        public string NomeSerieExerc { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public DateTime Horario { get; set; }
+
+        [Required]
+        public TipoExercicioEnum TipoExerc { get; set; }
+
+        [Required]
+        public decimal QuantidadeSemana { get; set; }
+
+        [Column(TypeName = "VARCHAR"), Required, MaxLength(1000), MinLength(10)]
+        public string DescricaoExerc { get; set; }
+
+        [Column(TypeName = "VARCHAR"), Required]
+        public bool StatusSistema { get; } = true;
+
+        [Required]
+        public int PacienteId { get; set; }
+
+        [Required]
+        public PacienteModel paciente { get; set; }
+
+        [Required]
+        public int UsuarioId { get; set; }
+        
+        [Required]
+        public UsuarioModel usuario { get; set; }    
+
+    }
+}
