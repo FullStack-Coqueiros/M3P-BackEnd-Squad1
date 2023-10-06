9a73a888f3745baebd81c81b444418a1799728c5
William Rocha <uill_pf@hotmail.com>
2023-10-05 21:14:26 -0300

    Correcao Endereco Enum com DataAnnotations

--- LabMedicineAPI/Model/Endereco.cs
+++ LabMedicineAPI/Model/Endereco.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +9,23 @@ namespace LabMedicineAPI.Model
 {
     public class Endereco
     {
+        [Key]
+        public int EnderecoId { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string CEP { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string Cidade { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string Estado { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string Logradouro { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string Numero { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string Complemento { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string Bairro { get; set; }
+        [Column(TypeName = "VARCHAR"), Required]
         public string PontoReferencia { get; set; }
 
 
