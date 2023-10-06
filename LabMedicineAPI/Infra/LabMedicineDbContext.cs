c5f269eddca9b404e0f8f2b3fc0ba9b43b87e326
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-06 20:22:29 -0300

    Validações paciente model

--- LabMedicineAPI/Infra/LabMedicineDbContext.cs
+++ LabMedicineAPI/Infra/LabMedicineDbContext.cs
@@ -1,6 +1,72 @@
-﻿namespace LabMedicineAPI.Infra
+﻿using LabMedicineAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LabMedicineAPI.Infra
 {
-    public class DbContext
+    public class LabMedicineDbContext : DbContext
     {
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<PacienteModel>Pacientes { get; set; }
+        public DbSet<ConsultaModel>Consultas { get; set; }
+        public DbSet<Endereco>Enderecos { get; set; }
+        public DbSet<ExameModel>Exames { get; set; }
+        public DbSet<ExercicioModel>Exercicios { get; set; }
+        public DbSet<DietaModel>Dietas { get; set; }
+        public DbSet<MedicamentoModel>Medicamentos { get; set; }
+
+
+
+        public LabMedicineDbContext(DbContextOptions<LabMedicineDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<PacienteModel>()
+                        .HasOne(h => h.Paciente) // Um usuário pode ter muitos pacientes
+                        .WithMany(w => w.Consultas) // Um paciente pertence a um único usuário
+                        .OnDelete(DeleteBehavior.Restrict);
+            
+            modelBuilder.Entity<UsuarioModel>()
+                        .HasOne(h => h.Pacientes) // Um usuário pode ter muitos pacientes
+                        .WithMany(w => w.Id) // Um paciente pertence a um único usuário
+                        .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Endereco>()
+                        .Property(p => p.DataCadastro)
+                        .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<ConsultaModel>()
+                        .Property(p => p.DataCadastro)
+                        .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<ExameModel>()
+                        .Property(p => p.DataCadastro)
+                        .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<ExercicioModel>()
+                        .Property(p => p.DataCadastro)
+                        .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<DietaModel>()
+                        .Property(p => p.DataCadastro)
+                        .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<MedicamentoModel>()
+                        .Property(p => p.DataCadastro)
+                        .HasDefaultValueSql("GETDATE()");
+
+            modelBuilder.Entity<UsuarioModel>()
+                       .HasMany(usuario => usuario.Pacientes) // Um usuário pode ter muitos pacientes
+                       .WithOne(paciente => paciente.Usuario) // Um paciente pertence a um único usuário
+                       .HasForeignKey(paciente => paciente.UsuarioId)
+                       .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
     }
 }
--- LabMedicineAPI/Program.cs
+++ LabMedicineAPI/Program.cs
@@ -1,3 +1,7 @@
+using LabMedicineAPI.Infra;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = "Server=DESKTOP-9HO92VC\\SQLEXPRESS;Database=labmedicinebd;Trusted_Connection=True;TrustServerCertificate=True;";
+builder.Services.AddDbContext<LabMedicineDbContext>(o => o.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
