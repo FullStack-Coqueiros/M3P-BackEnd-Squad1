e1d53ed8c6df189524a2d9f644139aed64670da0
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-06 09:29:54 -0300

    conflitos merge

--- LabMedicineAPI/Model/PacienteModel.cs
+++ LabMedicineAPI/Model/PacienteModel.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using LabMedicineAPI.Enums;
-using LabMedicineAPI.Model.Enums;
 using LabMedicineAPI.Model;
 using System.ComponentModel.DataAnnotations;
 
