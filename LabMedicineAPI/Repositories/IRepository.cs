3b32f8f711ffb6ed168a1f5d578726f8eb965d02
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-08 18:06:29 -0300

    criação iRepository

--- LabMedicineAPI/Repositories/IRepository.cs
+++ LabMedicineAPI/Repositories/IRepository.cs
@@ -2,5 +2,13 @@
 {
     public class IRepository
     {
+        public interface IRepository<TEntity> where TEntity : class
+        {
+            IEnumerable<TEntity> GetAll();
+            TEntity GetById(int id);
+            TEntity Create(TEntity entity);
+            TEntity Update(TEntity entity);
+            bool Delete(int id);
+        }
     }
 }
