6af8b78b313dd7e08f59fca7622d8c307612f429
alessandramedeirosvieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-10-10 20:44:00 -0300

    ajustesexame

--- LabMedicineAPI/DTOs/ExameUpdateDTO.cs
+++ LabMedicineAPI/DTOs/ExameUpdateDTO.cs
@@ -11,7 +11,6 @@ namespace LabMedicineAPI.DTOs
     public class ExameUpdateDTO
     {
         [Required(ErrorMessage = "O campo 'NomeExame' é obrigatório.")]
-        [StringLength(64, MinimumLength = 8, ErrorMessage = "O campo 'NomeExame' deve ter entre 8 e 64 caracteres.")]
         public string NomeExame { get; set; }
         
         [Required(ErrorMessage = "O campo 'Data' é obrigatório.")]
@@ -21,17 +20,14 @@ namespace LabMedicineAPI.DTOs
         public DateTime Horario { get; set; }
               
         [Required(ErrorMessage = "O campo 'TipoExame' é obrigatório.")]
-        [StringLength(32, MinimumLength = 4, ErrorMessage = "O campo 'TipoExame' deve ter entre 4 e 32 caracteres.")]
         public string TipoExame { get; set; }
 
         [Required(ErrorMessage = "O campo 'Laboratorio' é obrigatório.")]
-        [StringLength(32, MinimumLength = 4, ErrorMessage = "O campo 'Laboratorio' deve ter entre 4 e 32 caracteres.")]
         public string Laboratorio { get; set; }
 
         public string URL { get; set; }
 
         [Required(ErrorMessage = "O campo 'Resultados' é obrigatório.")]
-        [StringLength(1024, MinimumLength = 16, ErrorMessage = "O campo 'Resultados' deve ter entre 16 e 1024 caracteres.")]
         public string Resultados { get; set; }
 
         [Required(ErrorMessage = "O campo 'StatusSistema' é obrigatório.")]
@@ -40,21 +36,7 @@ namespace LabMedicineAPI.DTOs
         public int PacienteId { get; set; }
         [Required]
         public int UsuarioId { get; set; }
-
-       
-        public ExameUpdateDTO(string nomeExame, DateTime data, DateTime horario, string tipoExame, string laboratorio, string resultados, bool statusSistema, int pacienteId, int usuarioId)
-        {
-            NomeExame = nomeExame;
-            Data = data;
-            Horario = horario;
-            TipoExame = tipoExame;
-            Laboratorio = laboratorio;
-            Resultados = resultados; 
-            StatusSistema = statusSistema;
-            PacienteId = pacienteId;
-            UsuarioId = usuarioId;                   
-        }
-
+            
       
     }
 }
