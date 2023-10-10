93c71348ff0205afd80caddbb49b770e57bb4db4
William Rocha <uill_pf@hotmail.com>
2023-10-09 21:45:08 -0300

    AutoMapper configurado com Usuario Paciente Exame

--- LabMedicineAPI/AutoMapper/ConfigurationMapper.cs
+++ LabMedicineAPI/AutoMapper/ConfigurationMapper.cs
@@ -12,10 +12,34 @@ namespace LabMedicineAPI.AutoMapper
     {
         protected ConfigurationMapper()
         {
+            // ConfigurationMapper Usuario
+
             // Origem ... Destino
             CreateMap<UsuarioCreateDTO, UsuarioModel>();
             
-            
+            CreateMap<UsuarioGetDTO, UsuarioModel>();
+
+            CreateMap<UsuarioUpdateDTO, UsuarioModel>();
+
+            CreateMap<UsuarioDeleteDTO, UsuarioModel>();
+
+            // ConfigurationMapper Paciente
+
+            // Origem ... Destino
+            CreateMap<PacienteCreateDTO, PacienteModel>();
+
+            CreateMap<PacienteGetDTO, PacienteModel>();
+
+            CreateMap<PacienteUpdateDTO, PacienteModel>();
+
+            CreateMap<PacienteDeleteDTO, PacienteModel>();
+
+            // ConfigurationMapper Exame
+
+            // Origem ... Destino
+            CreateMap<ExameCreateDTO, ExameModel>();
+
+            CreateMap<ExameGetDTO, ExameModel>();
         }
     }
 }
