97d0a786b6bda4dc786a5c9ad1c04334cd2b1269
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-11 12:32:01 -0300

    criação dieta services

---
+++ LabMedicineAPI/Service/Dieta/IDietaServices.cs
@@ -0,0 +1,14 @@
+﻿using LabMedicineAPI.DTOs.Dieta;
+using LabMedicineAPI.Model;
+
+namespace LabMedicineAPI.Service.Dieta
+{
+    public interface IDietaServices
+    {
+        bool DeleteDieta(int id);
+        DietaModel DietaCreateDTO(DietaCreateDTO dietaCreateDTO);
+        DietaModel DietaUpdateDTO(int id, DietaUpdateDTO updateDietaDTO);
+        IEnumerable<DietaGetDTO> Get();
+        DietaGetDTO GetById(int id);
+    }
+}
