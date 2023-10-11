e9322d52210db9d186f8a3739d09b11554a7dfb1
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-11 14:10:32 -0300

    removendo conflito

--- LabMedicineAPI/Program.cs
+++ LabMedicineAPI/Program.cs
@@ -1,6 +1,5 @@
 using LabMedicineAPI.Infra;
 using LabMedicineAPI.Repositories;
-using LabMedicineAPI.Service.Dieta;
 using LabMedicineAPI.Service.Paciente;
 using LabMedicineAPI.Service.Usuario;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
@@ -23,7 +22,7 @@ builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddScoped<IPacienteServices, PacienteServices>();
 builder.Services.AddScoped<IUsuarioServices, UsuarioServices>();
-builder.Services.AddScoped<IDietaServices, DietaServices>();
+
 
 //ConfigurationMapper
 builder.Services.AddAutoMapper(typeof(Program));
