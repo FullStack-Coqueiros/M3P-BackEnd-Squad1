76309b69030d4fdcc552f38566c8b0d72659b999
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-11 12:20:33 -0300

    Create Exame Services

---
+++ LabMedicineAPI/Service/Exame/IExameServices.cs
@@ -0,0 +1,15 @@
+﻿using LabMedicineAPI.DTOs;
+using LabMedicineAPI.DTOs.Exame;
+using LabMedicineAPI.Model;
+
+namespace LabMedicineAPI.Service.Exame
+{
+    public interface IExameServices
+    {
+        bool DeleteExame(int id);
+        ExameModel ExameCreateDTO(ExameCreateDTO exameCreateDTO);
+        ExameModel exameUpdateDTO(int id, ExameUpdateDTO updateExameDTO);
+        IEnumerable<ExameGetDTO> Get();
+        ExameGetDTO GetById(int id);
+    }
+}
