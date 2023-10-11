f0fbfc3411e64303ee6351157c11db8560fdbc0a
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-11 13:40:18 -0300

    criação dtos endereço

---
+++ LabMedicineAPI/DTOs/Endereco/EnderecoGetDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace LabMedicineAPI.DTOs.Endereco
+{
+    public class EnderecoGetDTO
+    {
+        public string CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string PontoReferencia { get; set; }
+        public int UsuarioId { get; set; }
+        public int PacienteId { get; set; }
+
+    }
+}
