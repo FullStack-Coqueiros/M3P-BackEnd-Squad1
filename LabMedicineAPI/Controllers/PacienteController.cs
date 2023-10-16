f84c9f0d15548387570fd0c576c3bebb25b3561c
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-10-16 15:51:43 -0300

    create get all pacinete e get id paciente

---
+++ LabMedicineAPI/Controllers/PacienteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabMedicineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+    }
+}
